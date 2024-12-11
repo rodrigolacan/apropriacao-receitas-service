2c4a51490deafdb649f25019fc8d7f88d230b9cc
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-11 15:17:29 -0400

    feat: criando handler goblal para erros, evitando que o spring procure por recursos estáticos mvc

---
+++ src/main/java/rr/sebrae/apropriacao/app/exceptions/http/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package rr.sebrae.apropriacao.app.exceptions.http;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocorreu um erro interno: " + e.getMessage());
+    }
+}
+
