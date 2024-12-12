e1ddb242f8200ffcdfd38429b44c760de93b4a53
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-12 09:21:52 -0400

    feat: Definindo padrão de erro para 404

--- src/main/java/rr/sebrae/apropriacao/app/exceptions/http/GlobalExceptionHandler.java
+++ src/main/java/rr/sebrae/apropriacao/app/exceptions/http/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +15,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Ocorreu um erro interno: " + e.getMessage());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(NoHandlerFoundException ex) {
+        Map<String, String> errorResponse = Map.of(
+                "error", "Not Found",
+                "message", "The requested resource was not found",
+                "path", ex.getRequestURL()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
 
