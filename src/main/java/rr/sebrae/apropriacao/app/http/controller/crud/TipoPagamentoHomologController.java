5b257314c7dcd50d0a185d37b71dc45f40fb14b9
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-12 15:35:20 -0400

    feat: Implementando criação, listar todos e listar dados da tabela TipoPagamento por id.

--- src/main/java/rr/sebrae/apropriacao/app/http/controller/crud/TipoPagamentoHomologController.java
+++ src/main/java/rr/sebrae/apropriacao/app/http/controller/crud/TipoPagamentoHomologController.java
@@ -2,11 +2,17 @@ package rr.sebrae.apropriacao.app.http.controller.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.tipopagamento.dto.TipoPagamentoDTO;
 import rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.tipopagamento.entity.TipoPagamento;
 import rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.tipopagamento.service.TipoPagamentoService;
+import rr.sebrae.apropriacao.app.exceptions.http.ResourceNotFoundException;
+import rr.sebrae.apropriacao.app.http.responses.ResponseHandler;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Profile("dev")
@@ -18,10 +24,35 @@ public class TipoPagamentoHomologController {
     TipoPagamentoService tipoPagamentoService;
 
     @GetMapping("/tipopagamento")
-    public List<TipoPagamento> AlltipoPagamento() {
-       return tipoPagamentoService.findAllTipoPagamento();
+    public ResponseEntity<Object> allTipoPagamento() {
+        List<TipoPagamento> allTipoPagamento = tipoPagamentoService.findAllTipoPagamento();
+        return ResponseHandler.generateResponse("Successfully retrieved data!", HttpStatus.OK, allTipoPagamento);
     }
 
+    @GetMapping("/tipopagamento/{id}")
+    public ResponseEntity<Object> findTipoPagamentoById(@PathVariable int id) {
+        Optional<TipoPagamento> tipoPagamento = tipoPagamentoService.findTipoPagamentoById(id);
+        return tipoPagamento.map(tp -> ResponseHandler.generateResponse("Successfully retrieved data!", HttpStatus.OK, tp))
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado para id " + id));
+    }
+
+    @PostMapping("/tipopagamento")
+    public ResponseEntity<Object> createTipoPagamento(@RequestBody TipoPagamentoDTO tipoPagamentoDTO) {
+        TipoPagamento tipoPagamento = new TipoPagamento();
+        tipoPagamento.setNomeTipoPagamento(tipoPagamentoDTO.nomeTipoPagamento());
+
+        // Não passamos o ID manualmente, pois o banco irá gerar.
+        TipoPagamento savedTipoPagamento = tipoPagamentoService.saveTipoPagamento(tipoPagamento);
+
+        return ResponseHandler.generateResponse("Tipo de pagamento criado com sucesso!", HttpStatus.CREATED, savedTipoPagamento);
+    }
+
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 
 }
 
