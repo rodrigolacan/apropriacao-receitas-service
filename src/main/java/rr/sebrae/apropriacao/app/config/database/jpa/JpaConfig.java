07bc3690346b725dffd136d82f0652a5434a3a99
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-12 15:39:33 -0400

    feat: Configurando autidtoria no jpa para que ele alimente os campos created_at e update_at nas tabelas

---
+++ src/main/java/rr/sebrae/apropriacao/app/config/database/jpa/JpaConfig.java
@@ -0,0 +1,9 @@
+package rr.sebrae.apropriacao.app.config.database.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
