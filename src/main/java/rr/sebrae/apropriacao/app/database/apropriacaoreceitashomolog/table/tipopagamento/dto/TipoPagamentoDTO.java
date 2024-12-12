423da90937a383837e7a234105d5f46ddd808549
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-12 15:34:37 -0400

    feat: removendo do TipoPagamentoDTO o id pois esse utiliza sequências do postrgresql para gerar ids automaticamente

--- src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/tipopagamento/dto/TipoPagamentoDTO.java
+++ src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/tipopagamento/dto/TipoPagamentoDTO.java
@@ -3,7 +3,6 @@ package rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.tipo
 import java.util.Set;
 
 public record TipoPagamentoDTO(
-        Integer id,
         String nomeTipoPagamento,
         Set<Integer> viaReciboIds
 ) {
