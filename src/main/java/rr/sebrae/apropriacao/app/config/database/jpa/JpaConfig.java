f74de0a93a2da64dc659fbbf6c7c9bccaad55f9b
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-13 15:15:09 -0400

    feat: Criando Bean no JpaConfig para informar usuário de auditoria

--- src/main/java/rr/sebrae/apropriacao/app/config/database/jpa/JpaConfig.java
+++ src/main/java/rr/sebrae/apropriacao/app/config/database/jpa/JpaConfig.java
@@ -1,9 +1,17 @@
 package rr.sebrae.apropriacao.app.config.database.jpa;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("apropriacao.receitas");
+    }
 }
