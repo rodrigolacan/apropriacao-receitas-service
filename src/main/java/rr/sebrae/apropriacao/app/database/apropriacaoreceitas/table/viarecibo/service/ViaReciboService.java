798a3aa01df5519fc0cf55b7d6c8bd728fbede1e
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-13 16:08:40 -0400

    feat: implementando métodos de findById e save no service de ViaRecbio ambiente de produção

--- src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitas/table/viarecibo/service/ViaReciboService.java
+++ src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitas/table/viarecibo/service/ViaReciboService.java
@@ -3,20 +3,29 @@ package rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.s
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-import rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.repository.ViaReciboRepository;
 import rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.entity.ViaRecibo;
+import rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.repository.ViaReciboRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@Profile("pro")
+@Profile("prod")
 public class ViaReciboService {
 
     @Autowired
     private ViaReciboRepository viaReciboRepository;
 
-    public List<ViaRecibo> getAllViaRecibos() {
+    public List<ViaRecibo> findAllViaRecibos() {
         return viaReciboRepository.findAll();
+    }
+
+    public Optional<ViaRecibo> findViaReciboById(Integer id) {
+        return viaReciboRepository.findById(id);
+    }
+
+    public ViaRecibo saveViaRecibo(ViaRecibo viaRecibo) {
+        return viaReciboRepository.save(viaRecibo);
     }
 
 }
