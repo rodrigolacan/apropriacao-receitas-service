4dc501e38c0891d23023e4a79b16d37ded4c8672
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-13 09:00:04 -0400

    refact: Adicionando novamente id ao DTO para que possa ser possível implementar método update na tabela

--- src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/tipopagamento/dto/TipoPagamentoDTO.java
+++ src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/tipopagamento/dto/TipoPagamentoDTO.java
@@ -3,6 +3,7 @@ package rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.tipo
 import java.util.Set;
 
 public record TipoPagamentoDTO(
+        Integer id,
         String nomeTipoPagamento,
         Set<Integer> viaReciboIds
 ) {
