cad2508670e07d42d58f87671f9cc0a2eaa443ea
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-16 14:49:58 -0400

    refact: removendo a query personalizada pois o spring boot automaticamente verifica o campo deleted_at

--- src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitas/table/viarecibo/repository/ViaReciboRepository.java
+++ src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitas/table/viarecibo/repository/ViaReciboRepository.java
@@ -2,15 +2,13 @@ package rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.r
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rr.sebrae.apropriacao.app.database.apropriacaoreceitas.table.viarecibo.entity.ViaRecibo;
 
-import java.util.List;
+
 
 @Repository
 @Profile("prod")
 public interface ViaReciboRepository extends JpaRepository<ViaRecibo, Integer> {
-    @Query("SELECT vr FROM ViaRecibo vr WHERE vr.deletedAt IS NULL")
-    List<ViaRecibo> findAllActiveViaRecibos();
+
 }
