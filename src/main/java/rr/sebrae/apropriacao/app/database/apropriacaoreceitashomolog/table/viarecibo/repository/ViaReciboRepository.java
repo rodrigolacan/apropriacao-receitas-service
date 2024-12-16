eda39fc82bd5e4a3baf74ccd8626a630778aee9d
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-16 11:23:26 -0400

    feat: No respository, foi criado uma query personalizada para trazer todos os vias recibos que não estão com deleted_at setados

--- src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/viarecibo/repository/ViaReciboRepository.java
+++ src/main/java/rr/sebrae/apropriacao/app/database/apropriacaoreceitashomolog/table/viarecibo/repository/ViaReciboRepository.java
@@ -2,11 +2,16 @@ package rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.viar
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rr.sebrae.apropriacao.app.database.apropriacaoreceitashomolog.table.viarecibo.entity.ViaRecibo;
+
+import java.util.List;
 
 @Repository
 @Profile("dev")
 public interface ViaReciboRepository extends JpaRepository<ViaRecibo, Integer> {
+    @Query("SELECT vr FROM ViaRecibo vr WHERE vr.deletedAt IS NULL")
+    List<ViaRecibo> findAllActiveViaRecibos();
 
 }
