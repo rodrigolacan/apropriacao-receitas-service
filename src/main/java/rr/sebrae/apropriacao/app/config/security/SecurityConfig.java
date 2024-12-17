b2280cc5c4868c0fbb28b4dd7931bd4af4f2a4fd
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-12-17 10:53:24 -0400

    feat: implementando FilterChain do spring security para validar os tokens jwt dos clients cadastrados no keycloak

---
+++ src/main/java/rr/sebrae/apropriacao/app/config/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package rr.sebrae.apropriacao.app.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("https://lus-homolog.rr.sebrae.com.br/realms/microservices-homolog/protocol/openid-connect/certs")
+                        )
+                );
+
+        return http.build();
+    }
+}
